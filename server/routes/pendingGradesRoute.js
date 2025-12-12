3af8e1fd3c8a20bb631c9e9f82ff248a467f9773
jari <mohitjari09@gmail.com>
2025-12-12 21:14:15 +0530

    debug

--- server/routes/pendingGradesRoute.js
+++ server/routes/pendingGradesRoute.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const router = express.Router();
-const pendingGrades = require('../models/PendingGrades');
+const pendingGrades = require('../models/pendingGrades');
 
 router.get("/", async (req, res) => {
   try {
